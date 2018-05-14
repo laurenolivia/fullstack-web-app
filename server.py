3d7bc7c3745e32d0c803b4569eb05413e6e72890
laurenolivia <vagrant@vagrant.vm>
2018-05-14 19:56:04 +0000

    Building pages, routing requests

--- server.py
+++ server.py
@@ -17,12 +17,24 @@ def index():
     return render_template('homepage.html')
 
 #allow user to register info in db
-@app.route('/register', method=["GET"])
+@app.route('/register')
 def register_form():
     """requests username, password"""
 
+    return render_template('register.html')
 
-    return render_template('homepage.html')
+@app.route("/login")
+def prompt_login():
+    """Prompt user to login"""
+
+    return render_template("login.html")
+
+
+@app.route("/user_account")
+def display_account():
+    """Display user page"""
+
+    return render_template("user_account.html")
 
 
 
